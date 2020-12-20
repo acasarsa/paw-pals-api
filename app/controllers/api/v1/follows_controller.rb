68337b1281dd3d1b2e45db318e8ea21098491e67
github username <github email address>
2020-12-20 13:01:08 -0500

    clean up

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -3,33 +3,22 @@ class Api::V1::FollowsController < ApplicationController
     def show
         follow = Follow.find(params[:id])
         render json: follow, except: [:created_at, :updated_at]
-        # render json: follow, except: [:created_at, :updated_at], include: [:follower, :followee ], status: 201
-        # render json: FollowSerializer.new(follow).serializable_hash, status: 201
-        
     end
 
     def index 
         follows = Follow.all
         render json: follows, except: [:created_at, :updated_at]
-        # render json: FollowSerializer.new(follows)
     end
 
     def create 
         follow = Follow.create!(follow_params)
         render json:{follow_id: follow.id, followee: follow.followee, follower: follow.follower}, except: [:created_at, :updated_at], status: :ok
-        # amaze balls do i even need serializer??? 
-        # render json: { complex: complex, fields: fields, search_params: search_params }, status: :ok
-
     end
 
     def destroy
-        # byebug
         follow = Follow.find(params[:id])
-        # byebug
         follow.destroy
-        # byebug
         render json: {id: params[:id]}
-        # byebug
     end
 
     private
